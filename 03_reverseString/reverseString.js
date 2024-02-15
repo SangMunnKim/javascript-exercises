c3ab440942600bbacfe1e71f92cd28ab8d5764aa
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-02-15 11:34:34 +0100

    Add solved 03_reverseString and 04_removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringToReturn = "";
+    for (let i=1; i <= string.length; i++) {
+        stringToReturn += string[string.length-i]
+    }
+    return stringToReturn;
 };
 
 // Do not edit below this line
