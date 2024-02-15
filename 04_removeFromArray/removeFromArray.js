ea17d40c5e1eaa6f67c51efe7934c40fe45cbf4d
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-02-15 22:11:46 +0100

    Add solution ex. 5,6,7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,16 @@
+//Alternative 1
+// const removeFromArray = function(array, ...args) {
+//     const newArray = [];
+//     for (item of array) {
+//         if (!args.includes(item)) {
+//             newArray.push(item)
+//         }
+//     }
+//     return newArray;
+// };
+//Alternative 2
 const removeFromArray = function(array, ...args) {
-    const newArray = [];
-    for (item of array) {
-        if (!args.includes(item)) {
-            newArray.push(item)
-        }
-    }
+    const newArray = array.filter((item) => (!args.includes(item)));
     return newArray;
 };
 
